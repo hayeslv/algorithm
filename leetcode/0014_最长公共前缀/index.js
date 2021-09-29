789da916ce0bfb4cc1da39f733fc63caa01076e9
lvhaizhou <417703682@qq.com>
2021-09-29 16:44:32 +0800

    最长公共前缀

--- leetcode/0014_最长公共前缀/index.js
+++ leetcode/0014_最长公共前缀/index.js
@@ -5,3 +5,18 @@
  * 编写一个函数来查找字符串数组中的最长公共前缀。
  * 如果不存在公共前缀，返回空字符串 ""。
  */
+
+var longestCommonPrefix = function(strs) {
+  if(strs === null || strs.length === 0) return ''
+  let prevs = strs[0]
+  for(let i=1; i<strs.length; i++) {
+    let j=0
+    for(; j<prevs.length && j<strs[i].length; j++) {
+      if(prevs.charAt(j) !== strs[i].charAt(j)) break
+    }
+    prevs = prevs.substring(0, j)
+    if(prevs === '') return ''
+  }
+  return prevs
+}
+
