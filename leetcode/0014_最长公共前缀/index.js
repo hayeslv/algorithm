096c5e54fad73661cbee3cfd8db00011dfdf85cb
lvhaizhou <417703682@qq.com>
2021-10-09 14:46:23 +0800

    最长公共前缀：Trie树

--- leetcode/0014_最长公共前缀/index.js
+++ leetcode/0014_最长公共前缀/index.js
@@ -63,3 +63,50 @@ function lcPrefixTwo(str1, str2) {
   }
   return str1.substring(0, j)
 }
+
+// 解法四：Trie树（字典树）
+var longestCommonPrefix4 = function(strs) {
+  if(strs === null || strs.length === 0) return ''
+  // 初始化Trie树
+  let trie = new Trie()
+  // 构建Trie树
+  for(let i=0; i<strs.length; i++) {
+    if(!trie.insert(strs[i])) return ''
+  }
+  // 返回最长公共前缀
+  return trie.searchLongestPrefix()
+}
+// Trie树
+var Trie = function() {
+  this.root = new TrieNode()
+}
+var TrieNode = function() {
+  // next放入当前节点的子节点
+  this.next = {};
+  // 当前是否是结束节点
+  this.isEnd = false;
+}
+Trie.prototype.insert = function(word) {
+  if(!word) return false;
+  let node = this.root;
+  for(let i=0; i<word.length; i++) {
+    if(!node.next[word[i]]) {
+      node.next[word[i]] = new TrieNode()
+    }
+    node = node.next[word[i]]
+  }
+  node.isEnd = true
+  return true
+}
+Trie.prototype.searchLongestPrefix = function() {
+  let node = this.root
+  let prevs = ''
+  while(node.next) {
+    let keys = Object.keys(node.next)
+    if(keys.length !== 1) break
+    prevs += keys[0]
+    if(node.next[keys[0]].isEnd) break
+    node = node.next[keys[0]]
+  }
+  return prevs
+}
